7a5c3ccba50b99fd400acc9ce323b074eb07ca1b
MatthieuDartiailh <marul@laposte.net>
2018-10-05 13:31:18 -0400

    version: cut first release

--- shabanipy/version.py
+++ shabanipy/version.py
@@ -26,7 +26,7 @@ MINOR = 1
 MICRO = 0
 
 # The status indicate if this is a development or pre-release version
-STATUS = 'dev'
+STATUS = ''
 
 #: A namedtuple of the version info for the current release.
 version_info = namedtuple('version_info', 'major minor micro status')
