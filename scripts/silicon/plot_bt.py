23956c46b05c4f8e363ad74850e989328171940a
kasramat <kasra.sardashti@gmail.com>
2019-02-24 22:42:12 -0500

    Updated for multiple maps in one dataset. Changed the variables names to make the parameters clear to track.

--- scripts/silicon/plot_bt.py
+++ scripts/silicon/plot_bt.py
@@ -15,18 +15,19 @@ The plot use the folling axes:
 #: Path towards the hdf5 file holding the data
 PATH = '/Users/kasrasardashti/Labber/Data/2019/02/Data_0209/KS7_KS8_JS221_JY001_005.hdf5'
 
-#: Name or index of the column containing the power data
-TEMPERATURE_NAME = 1
+#: Name or index of the column containing the temperature data
+TEMPERATURE_NAME = 'Oxford Mercury iTC - Sensor2.TargetTemperature'
 
-#: Name or index of the column containing the voltage data
-FIELD_NAME = 0
+#: Name or index of the column containing the field data
+MAGFIELD_NAME = 'Magnet - Magnetic Field'
 
-#: Name or index of the column containing the current data
-#: This should be a stepped channel ! use the applied voltage not the
-#: measured current
-RESISTANCE_NAME = 2
+#: Name or index of the column containing the resistance data
+#: This should be a stepped channel ! use the measured voltage from lock-in
+RESISTANCE_NAME_1 = 'Lockin-1 - Value'
+RESISTANCE_NAME_2 = 'Lockin-2 - Value'
+RESISTANCE_NAME_3 = 'Lockin-3 - Value'
 
-#: Label of the x axis, if left blanck the power column name will be used
+#: Label of the x axis, if left blanck the field column name will be used
 #: If an index was passed the name found in the labber file is used.
 X_AXIS_LABEL = 'Magnetic Field (B)'
 
@@ -56,16 +57,16 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-from shabanipy.shapiro.binning import bin_power_shapiro_steps
 from shabanipy.utils.labber_io import LabberData
 
 with LabberData(PATH) as data:
 
-    shape = data.compute_shape((FIELD_NAME, TEMPERATURE_NAME))
+    shape = data.compute_shape((MAGFIELD_NAME, TEMPERATURE_NAME))
 
-    field = data.get_data(FIELD_NAME)
+    field = data.get_data(MAGFIELD_NAME)
     temp = data.get_data(TEMPERATURE_NAME)
-    cond = np.abs(data.get_data(RESISTANCE_NAME))
+    resist_1 = np.abs(data.get_data(RESISTANCE_NAME_1))
+    resist_2 = np.abs(data.get_data(RESISTANCE_NAME_2))
 
     # Handle interruptions in the last scan.
     while len(field) < shape[0]*shape[1]:
@@ -74,13 +75,31 @@ with LabberData(PATH) as data:
     length = shape[0]*shape[1]
     field = field[:length].reshape(shape)
     temp = temp[:length].reshape(shape)
-    cond = cond[:length].reshape(shape).T
-    for i in range(1, cond.shape[0], 2):
-        cond[i, :] = cond[i, ::-1]
+    resist_1 = resist_1[:length].reshape(shape).T
+    resist_2 = resist_2[:length].reshape(shape).T
+
+    for i in range(1, resist_1.shape[0], 2):
+        resist_1[i, :] = resist_1[i, ::-1]
+        resist_2[i, :] = resist_2[i, ::-1]
 
 
 # Plot the data
-plt.imshow(C_SCALING * cond,
+plt.figure(1)
+plt.imshow(C_SCALING * resist_1,
+           extent=(field[0, 0], field[-1, 0], temp[0, 0], temp[0, -1]),
+           origin='lower',
+           aspect='auto',
+           vmin=C_LIMITS[0],
+           vmax=C_LIMITS[1])
+plt.xlabel(X_AXIS_LABEL)
+plt.xlim(X_LIMITS)
+plt.ylabel(Y_AXIS_LABEL)
+plt.ylim(Y_LIMITS)
+cbar = plt.colorbar()
+cbar.ax.set_ylabel(C_AXIS_LABEL)
+
+plt.figure(2)
+plt.imshow(C_SCALING * resist_2,
            extent=(field[0, 0], field[-1, 0], temp[0, 0], temp[0, -1]),
            origin='lower',
            aspect='auto',
