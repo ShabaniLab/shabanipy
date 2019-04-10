b121713d7558a98e12c021737bfcfd613d1baead
MatthieuDartiailh <marul@laposte.net>
2019-04-10 09:39:10 -0400

    scripts.shapiro: minor changes to single dataset step weight analysis

--- scripts/shapiro/shapiro_step_weight.py
+++ scripts/shapiro/shapiro_step_weight.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Generate a color plot of the weight of a shapiro steps.
 
-The plot is done as a function of power and frequency and the power is 
+The plot is done as a function of power and frequency and the power is
 normalized by the power at which the step 0 disappear.
 
 The plot use the folling axes:
@@ -19,7 +19,7 @@ The plot use the folling axes:
 PATH = r'/Users/mdartiailh/Labber/Data/2018/09/Data_0913/JS124L_CD004_009.hdf5'
 
 #: Index of the setps for which to generate a plot.
-STEP_INDEXES = [-2, -1, 0, 1, 2, 3, 4]
+STEP_INDEXES = [0, 1, 2, 3, 4]
 
 #: Name or index of the column containing the frequency data if applicable.
 #: Leave blanck if the datafile does not contain a frequency sweep.
@@ -94,7 +94,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from shabanipy.shapiro import normalize_db_power
-from shabanipy.shapiro.binning import (bin_power_shapiro_steps, 
+from shabanipy.shapiro.binning import (bin_power_shapiro_steps,
                                        extract_step_weight)
 from shabanipy.utils.labber_io import LabberData
 
@@ -104,8 +104,8 @@ with LabberData(PATH) as data:
     grid_f = []
     grid_p = []
     step_counts = {s_i: [] for s_i in STEP_INDEXES}
-    
-    for frequency in frequencies:        
+
+    for frequency in frequencies:
         filters = {}
         filters[FREQUENCY_NAME] = frequency
 
@@ -133,7 +133,7 @@ with LabberData(PATH) as data:
 
         # Bin the data
         power, voltage, histo = bin_power_shapiro_steps(power, curr, volt,
-                                                        frequency, 
+                                                        frequency,
                                                         STEP_FRACTION)
 
         # Find the normalizing power
@@ -160,10 +160,10 @@ tri_p = np.concatenate(grid_p)*X_SCALING
 
 # Plot the data
 for step_index in step_counts:
-    
+
     plt.figure()
     weights = np.concatenate(step_counts[step_index])
-    plt.tricontourf(tri_p, tri_f, 
+    plt.tricontourf(tri_p, tri_f,
                     weights*C_SCALING,
                     vmin=C_LIMITS[0],
                     vmax=C_LIMITS[1])
@@ -192,9 +192,9 @@ for step_index in step_counts:
         weights_next = np.concatenate(step_counts[associated_index])
         # Get the indexes at which the denominator does not vanish
         indexes = np.nonzero(weights_next)
-        # Compute the ratio 
+        # Compute the ratio
         ratio = weights[indexes]/weights_next[indexes]
-        plt.tricontourf(tri_p[indexes], tri_f[indexes], 
+        plt.tricontourf(tri_p[indexes], tri_f[indexes],
                         ratio, levels=np.arange(0, 2.0, 0.25),
                         vmin=0,
                         vmax=2)
@@ -206,7 +206,7 @@ for step_index in step_counts:
         plt.scatter(tri_p[indexes], tri_f[indexes], marker='1', color='k')
 
         sample = (PATH.rsplit(os.sep, 1)[1]).split('_')[0]
-        plt.title(f'Sample {sample}: $Q_{{%d, %d}}$' % 
+        plt.title(f'Sample {sample}: $Q_{{%d, %d}}$' %
                   (step_index, associated_index))
         plt.xlabel(X_AXIS_LABEL)
         plt.ylabel(Y_AXIS_LABEL)
