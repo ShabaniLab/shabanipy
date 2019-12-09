1354ff3d65dde2621fcd4bdc7275fddfca7d6d56
Noah Goss <goss@10-17-122-104.dynapool.wireless.nyu.edu>
2019-12-09 14:59:09 -0500

    added photon number code

--- scripts/cavities/notch_fit_shabani.py
+++ scripts/cavities/notch_fit_shabani.py
@@ -17,16 +17,25 @@ from shabanipy.cavities.utils import (extract_baseline,
 PATH = '/Users/goss/Desktop/Shabani/data/IBM_Resonators/IBM_resonators_JY002_006.hdf5'
 FREQ_COLUMN = 0
 POWER_COLUMN = 1
+ATTENUATION_ON_VNA = 0
 PLOT_Q_VS_POWER = True
+PLOT_PHOTON_VS_Q = True
+GET_PHOTON_NUMBER = True
+GET_SINGLE_PHOTON_LIMIT = True
+
+
+def calculate_total_power(freq,power):
+    totalPower = (-(1/800000000)*freq - (215/4)) + power + ATTENUATION_ON_VNA
+    return totalPower
 
 #uncommentresonance parameters line for 0th resonance, 1st resonance, and so on.
 RESONANCE_PARAMETERS = {
-0: ('min', 51, 500, 1e13, 'amplitude', 'nelder'),
+#0: ('min', 51, 500, 1e13, 'amplitude', 'nelder'),
 #1: ('min', 51, 500, 1e13, 'amplitude', 'nelder'),
 #2: ('min', 51, 500, 1e13, 'amplitude', 'nelder'),
-#3: ('min', 51, 500, 1e13, 'amplitude', 'nelder'),
-    #4: ('max', 51, 1000, 0, 'amplitude', 'leastsq'),
-    # 5: ('max', 51, 400, 1e13, 'amplitude', 'leastsq'),
+3: ('min', 51, 500, 1e13, 'amplitude', 'nelder'),
+#4: ('max', 51, 1000, 0, 'amplitude', 'nelder'),
+# 5: ('max', 51, 400, 1e13, 'amplitude', 'leastsq'),
     }
 
 with LabberData(PATH) as data:
@@ -45,6 +54,7 @@ for res_index, res_params in RESONANCE_PARAMETERS.items():
     
     powerList = []
     qList = []
+    photonList = []
     kind, sav_f, e_delay, base_smooth, method, fit_method = res_params
     for p_index, power in enumerate(powers):
         
@@ -94,16 +104,31 @@ for res_index, res_params in RESONANCE_PARAMETERS.items():
         port1.GUIfit()
         
         port1.plotall()
-
-        print("at power ")
-        print(power)
+        
+        print("at power " + str(power) + " and " + str(ATTENUATION_ON_VNA) + " attenuation:")
         powerList.append(float(power))
         fit = pd.DataFrame([port1.fitresults]).applymap(lambda x: "{0:.2e}".format(x))
         display(fit)
         qList.append(float(fit.iat[0,1]))
         
+        if GET_PHOTON_NUMBER == True:
+            photonNum = port1.get_photons_in_resonator(calculate_total_power(fc,power),'dBm')
+            photonList.append(float(photonNum))
+            print("There are " + str(photonNum) + " photons in the cavity")
+        if GET_SINGLE_PHOTON_LIMIT == True:
+            singlePhotonLim = port1.get_single_photon_limit()
+            print("Power for single photon limit: " + str(singlePhotonLim))
+
     if PLOT_Q_VS_POWER == True:
         plt.plot(powerList,qList)
-        plt.title("Power Vs. Qi")
+        plt.title("Power Vs. Qi for resonance at " +str(fc) +"Hz")
+        plt.xlabel("power [dB]")
+        plt.ylabel("internal quality factor")
         plt.show()
     
+    if PLOT_PHOTON_VS_Q == True:
+        plt.plot(photonList,qList)
+        plt.title("Photon Number Vs. Qi for resonance at " + str(fc) +"Hz")
+        plt.xlabel("photon number")
+        plt.ylabel("internal quality factor")
+        plt.show()
