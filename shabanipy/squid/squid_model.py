4dfe00844aa3139d89dbb31c771e424ec4ed6abf
MatthieuDartiailh <marul@laposte.net>
2020-03-16 17:33:42 -0400

    squid: add the possibility to take into account an inductance

--- shabanipy/squid/squid_model.py
+++ shabanipy/squid/squid_model.py
@@ -9,11 +9,24 @@
 """Model functions used to describe SQUID oscillations.
 
 """
+from math import pi
+
 import numpy as np
+from scipy.constants import h, e
+from scipy.interpolate import interp1d
 
 
-def compute_squid_current(phase, cpr1, parameters1, cpr2, parameters2,
-                          positive=True, aux_res=101):
+def compute_squid_current(
+    phase,
+    cpr1,
+    parameters1,
+    cpr2,
+    parameters2,
+    positive=True,
+    aux_res=101,
+    inductance=0.0,
+    compute_phase=False,
+):
     """Compute the SQUID current from 2 CPRs.
 
     Parameters
@@ -30,17 +43,63 @@ def compute_squid_current(phase, cpr1, parameters1, cpr2, parameters2,
         callable should take the phase as first argument.
     parameters1 : tuple
         Parameters to use to compute the current in the second junction.
+    positive : bool, optional
+        Should the computed current be positive or negative
+    aux_res : int, optional
+        Number of points to use when optimizing the phase to get the maximum
+        current.
+    inductance : float, optional
+        Inductance of the loop in H
+    compute_phase : bool, optional
+        Computethe phase at which the squid current is maximal instead of the
+        current.
 
     """
-    aux = np.tile(np.linspace(0, 2*np.pi, aux_res), (len(phase), 1))
     phi1, *p1 = parameters1
-    phi1 += (aux.T + phase).T
     phi2, *p2 = parameters2
-    phi2 += aux
-    cp1 = cpr1(phi1, *p1)
-    cp2 = cpr2(phi2, *p2)
-    total_current = cp1 + cp2
-    if positive:
-        return np.max(total_current, axis=-1)
+    if inductance == 0.0:
+        aux = np.tile(np.linspace(0, 2 * np.pi, aux_res), (len(phase), 1))
+        phi1 += (aux.T + phase).T
+        phi2 += aux
+        cp1 = cpr1(phi1, *p1)
+        cp2 = cpr2(phi2, *p2)
+        total_current = cp1 + cp2
+        if positive:
+            if compute_phase:
+                index = np.argmax(total_current, axis=-1)
+                return aux[0, index]
+            return np.max(total_current, axis=-1)
+        else:
+            if compute_phase:
+                index = np.argmin(total_current, axis=-1)
+                return aux[0, index]
+            return np.min(total_current, axis=-1)
+
     else:
-        return np.min(total_current, axis=-1)
+        # In the presence of an inductance compute first the phase giving the
+        # larger current as a function of the phase difference between the two
+        # JJ.
+        delta_phi = np.linspace(2 * phase[0], 2 * phase[-1], 2 * len(phase) * 10)
+        optimal_aux = compute_squid_current(
+            delta_phi,
+            cpr1,
+            parameters1,
+            cpr2,
+            parameters2,
+            positive,
+            aux_res,
+            0.0,
+            True,
+        )
+
+        # Next compute the external phase that gave rise to the phase
+        # difference
+        phi1 += delta_phi + optimal_aux
+        phi2 += optimal_aux
+        cp1 = cpr1(phi1, *p1)
+        cp2 = cpr2(phi2, *p2)
+        total_current = cp1 + cp2
+        phi_ext = delta_phi + 2 * pi * e / h * inductance * (cp2 - cp1)
+        # Interpolate the current as a function of the external phase and
+        # compute the current at the requested points
+        return interp1d(phi_ext, total_current, kind="cubic", copy=False)(phase)
