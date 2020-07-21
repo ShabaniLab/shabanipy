caf6e6412d46d4cdcb0e752bd2adcbb5d7346429
wfsch <bschiela@gmail.com>
2020-07-20 20:39:04 -0400

    Black and flake8 cleanup

--- scripts/jj/fraunhofer/multigate.py
+++ scripts/jj/fraunhofer/multigate.py
@@ -1,7 +1,6 @@
 """Plot multigate Fraunhofers and guess corresponding current densities."""
 import os
 from pathlib import Path
-import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,7 +8,8 @@ from scipy import constants as cs
 
 from shabanipy.jj.fraunhofer.generate_pattern import produce_fraunhofer_fast
 from shabanipy.jj.fraunhofer.deterministic_reconstruction import (
-        extract_theta, extract_current_distribution)
+    extract_current_distribution,
+)
 from shabanipy.jj.fraunhofer.utils import (
     find_fraunhofer_center,
     symmetrize_fraunhofer,
@@ -24,8 +24,10 @@ LABBER_DATA_DIR = os.environ['LABBER_DATA_DIR']
 # Gates 2 and 4 are fixed at Vg2 = Vg4 = constant while gates 1, 3, and 5 are
 # swept together.
 # A Fraunhofer pattern is measured for 6 values of Vg1 = Vg3 = Vg5.
-DATA_FILE_PATH = (Path(LABBER_DATA_DIR) /
-        '2019/11/Data_1104/JS123A_BM003_054.hdf5')
+DATA_FILE_PATH = (
+    Path(LABBER_DATA_DIR) /
+    '2019/11/Data_1104/JS123A_BM003_054.hdf5'
+)
 
 # channel names
 CH_MAGNET = 'Keithley Magnet 1 - Source current'
@@ -54,8 +56,14 @@ with LabberData(str(DATA_FILE_PATH)) as f:
     gate = np.unique(f.get_data(CH_GATE))
     for g in gate:
         bias = f.get_data(CH_BIAS, filters={CH_GATE: g})[:-10]
-        resist.append(np.abs(np.real(VOLT_TO_RESIST *
-            f.get_data(CH_RESIST, filters={CH_GATE: g})[:-10])))
+        resist.append(
+            np.abs(
+                np.real(
+                    VOLT_TO_RESIST
+                    * f.get_data(CH_RESIST, filters={CH_GATE: g})[:-10]
+                )
+            )
+        )
         ic.append(extract_switching_current(bias, resist[-1], 5, 'positive'))
 resist = np.array(resist)
 ic = np.array(ic)
