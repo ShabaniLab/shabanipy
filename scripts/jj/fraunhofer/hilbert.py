c4302976d0587de0648c83d1b8691918a0f06b2e
wfsch <bschiela@gmail.com>
2020-08-27 18:56:24 -0400

    Start script to benchmark Hilbert transform implementation
    At present, this just performs a sanity check that scipy.signal.hilbert
    and shabanipy...extract_theta are equivalent, despite having to
    exponentiate the function passed to extract_theta (which subsequently
    takes the logarithm).

---
+++ scripts/jj/fraunhofer/hilbert.py
@@ -0,0 +1,34 @@
+"""Test Hilbert transform implementation against a known analytical example."""
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.signal import hilbert
+
+from shabanipy.jj.fraunhofer.deterministic_reconstruction import extract_theta
+
+
+def f(x):
+    """A function with an analytical Hilbert transform."""
+    return 1 / (x ** 2 + 1)
+
+
+def hilbert_f(x):
+    """The Hilbert transform of f."""
+    return x / (x ** 2 + 1)
+
+
+xs = np.linspace(-10, 10, 105)
+
+fig, ax = plt.subplots(constrained_layout=True)
+ax.set_title(r"Hilbert transform of $\frac{1}{x^2 + 1}$")
+ax.plot(xs, hilbert_f(xs), label=r"$\frac{x}{x^2+1}$")
+ax.plot(xs, hilbert(f(xs)).imag, lw=0, marker="o", label="scipy.signal.hilbert")
+# n.b. extract_theta computes the hilbert transform of the logarithm
+ax.plot(
+    xs,
+    extract_theta(xs, np.exp(f(xs)), 1, 0, method="hilbert"),
+    lw=0,
+    marker=".",
+    label="shabanipy extract_theta",
+)
+ax.legend()
+plt.show()
