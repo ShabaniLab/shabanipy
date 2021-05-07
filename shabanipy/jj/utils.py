fd8250d3e9e28fc9d804e37b8e31956b25d85ca6
Bassel Heiba Elfeky <bhe220@nyu.edu>
2021-05-07 12:29:20 -0400

    Extract switching current modification
    Changing correct voltage offset parameter for switching current extraction

--- shabanipy/jj/utils.py
+++ shabanipy/jj/utils.py
@@ -226,8 +226,7 @@ def extract_switching_current(
 
     """
     # Correct of the DMM voltage offset(superconducting region should be around zero)
-    volt_or_res = correct_voltage_offset(bias,volt_or_res,2)
-
+    volt_or_res = correct_voltage_offset(bias,volt_or_res,1)
     if side not in ("positive", "negative"):
         raise ValueError(f"Side should be 'positive' or 'negative', found {side}.")
 
