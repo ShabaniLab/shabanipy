69340d15e800eafd18c660cd5339a0e4571e75d2
wfsch <bschiela@gmail.com>
2021-11-15 13:58:53 -0500

    Relabel axes and rename file

--- scripts/squid/plot_cpr_vs_transparency.py
+++ scripts/squid/plot_cpr_vs_transparency.py
@@ -10,11 +10,11 @@ transparency = np.linspace(0, 0.9, 10)
 transparency = np.append(transparency, [0.9999])
 
 fig, ax = plt.subplots()
-ax.set_xlabel("phase [$\pi$]")
-ax.set_ylabel("current [$I_c$]")
+ax.set_xlabel("phase [$2\pi$]")
+ax.set_ylabel("$I_c$ [arb.u.]")
 for tau in transparency:
     lines = ax.plot(
-        phase / np.pi,
+        phase / (2 * np.pi),
         finite_transparency_jj_current(phase, 1, tau),
         label=f"$\\tau$ = {round(tau, 2)}",
         color=plt.get_cmap("viridis")(tau),
