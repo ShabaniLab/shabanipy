d1f896274cf48ac56e089a3f9c3f12ffd1ab9f7e
smfarzaneh <smf1992@gmail.com>
2021-12-14 17:27:13 -0500

    add scipy to the package requirements

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(filename):
         return f.read().decode('utf8')
 
 
-requirements = ['numpy', 'h5py', 'matplotlib', 'lmfit']
+requirements = ['numpy', 'h5py', 'matplotlib', 'lmfit', 'scipy']
 
 
 setup(name='shabanipy',
