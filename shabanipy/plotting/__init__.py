40b2f2f1b3458bd4d7716ab9ea697c9aa1e51a88
William Schiela <bschiela@gmail.com>
2022-02-23 18:09:12 -0500

    Add utility for plotting Labber .hdf5 data

--- shabanipy/plotting/__init__.py
+++ shabanipy/plotting/__init__.py
@@ -1,2 +1,2 @@
-from .plot import plot, plot2d
+from .plot import plot, plot2d, plot_labberdata
 from .utils import stamp
--- shabanipy/plotting/plot.py
+++ shabanipy/plotting/plot.py
@@ -4,8 +4,13 @@ These functions only handle creation of the matplotlib objects and plotting of t
 All plot formatting/styling should be left to the package consumer (ideally controlled
 by an mplstyle sheet).
 """
+from pathlib import Path
+from typing import Callable, Dict, List, Optional, Tuple, Union
+
 import numpy as np
-from matplotlib.pyplot import subplots
+from matplotlib import pyplot as plt
+
+from shabanipy.labber import LabberData, get_data_dir
 
 from .utils import stamp as sp_stamp
 
@@ -124,8 +129,94 @@ def plot2d(
     return fig, ax
 
 
+def plot_labberdata(
+    path: Union[str, Path],
+    x: str,
+    y: str,
+    z: str,
+    xlabel: Optional[str] = None,
+    ylabel: Optional[str] = None,
+    zlabel: Optional[str] = None,
+    xtransform: Optional[Callable] = None,
+    ytransform: Optional[Callable] = None,
+    ztransform: Optional[Callable] = None,
+    title: Optional[str] = None,
+    ax: Optional[plt.Axes] = None,
+    style: Union[str, Dict, Path, List] = "default",
+    **kwargs
+) -> Tuple[plt.Figure, plt.Axes]:
+    """Plot data from a Labber .hdf5 log file.
+
+    Parameters
+    ----------
+    path
+        The path to data file. If not absolute, it should be relative to the output of
+        `shabanipy.labber.get_data_dir`.
+    x, y, z
+        Channel names or vector channel `x_name`s specifying which data to plot.
+    xlabel, ylabel, zlabel
+        Axes labels.  If None, the names from `x`, `y`, and `z` will be used.
+    xtransform, ytransform, ztransform
+        Functions with signature `np.ndarray -> np.ndarray` used to transform the data.
+    title
+        Plot title.
+    ax
+        The matplotlib Axes in which to plot.
+    style
+        Matplotlib style specifications passed to `matplotlib.pyplot.style.use`.
+    **kwargs
+        Additional keyword arguments passed to the plotting function
+        (`shabanipy.plotting.plot2d` in the case of 2d color plots).
+
+    Returns
+    -------
+    The figure and axes where the data were plotted.
+    """
+    path = Path(path)
+    if not path.is_absolute():
+        path = get_data_dir() / path
+
+    # get the data
+    data = []
+    with LabberData(path) as f:
+        for name in (x, y, z):
+            try:
+                data.append(f.get_data(name))
+            except ValueError:
+                # TODO refactor LabberData.get_data to normalize data access
+                for log in (l for l in f.logs if l.x_name == name):
+                    vdata, _ = f.get_data(log.name, get_x=True)
+                    data.append(vdata)
+                    break
+
+    # normalize shapes against potentially vectorial data
+    dims = [d.ndim for d in data]
+    max_dim = max(dims)
+    for i, d in enumerate(data):
+        if d.ndim < max_dim:
+            data[i] = np.expand_dims(d, axis=tuple(-np.arange(1, 1 + max_dim - d.ndim)))
+    data = np.broadcast_arrays(*data)
+
+    # apply transformations
+    for i, (d, transform) in enumerate(zip(data, (xtransform, ytransform, ztransform))):
+        if transform is not None:
+            data[i] = transform(data[i])
+
+    # plot
+    plt.style.use(style)
+    return plot2d(
+        *data,
+        # TODO: automatically add units in the default case
+        xlabel=xlabel if xlabel is not None else x,
+        ylabel=ylabel if ylabel is not None else y,
+        zlabel=zlabel if zlabel is not None else z,
+        title=title,
+        **kwargs
+    )
+
+
 def _fig_ax(ax=None):
     if ax is None:
-        return subplots()
+        return plt.subplots()
     else:
         return ax.get_figure(), ax
