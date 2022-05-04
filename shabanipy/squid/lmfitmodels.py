ShabaniLab/shabanipy

commit bd4321ed4e604250fe0ffc4de6012367647e46ad

Change radians_per_tesla -> loop_area in model func

shabanipy/squid/lmfitmodels.py
@@ -18,7 +18,7 @@ PHI0 = physical_constants["mag. flux quantum"][0]
 def squid_model(
     bfield: np.ndarray,
     bfield_offset: float,
-    radians_per_tesla: float,
+    loop_area: float,
     anomalous_phase1: float,
     anomalous_phase2: float,
     critical_current1: float,
@@ -40,8 +40,9 @@ def squid_model(
         External magnetic field (T) at which the critical current was measured.
     bfield_offset
         Magnetic field offset (T) due to hysteresis in the vector magnet coils.
-    radians_per_tesla
-        Externally applied phase per unit field, 2πA/Φ0, where A is the loop area.
+    loop_area
+        Effective area A (m^2) of the SQUID loop, used to convert the external field B
+        to phase via 2πAB/Φ0.
     anomalous_phase1, anomalous_phase2
         Anomalous shifts in the junctions' phases (rad).
     critical_current1, critical_current2
@@ -73,7 +74,7 @@ def squid_model(
         if inductance == 0
         else partial(critical_control, ninterp=ninterp)
     )
-    phase_ext = (bfield - bfield_offset) * radians_per_tesla
+    phase_ext = 2 * np.pi * loop_area * (bfield - bfield_offset) / PHI0
     squid_ic = [
         critical(
             phase_ext,

scripts/squid/fit_squid_oscillations.py
@@ -215,7 +215,7 @@ else:
 # create the model
 param_specs = {
     "bfield_offset": None,
-    "radians_per_tesla": {"min": 0},
+    "loop_area": {"min": 0},
     "anomalous_phase1": {"value": 0, "vary": False},
     "anomalous_phase2": {"value": 0, "vary": False},
     "critical_current1": {"min": 0},
@@ -311,11 +311,8 @@ if args.fraunhofer == "fit":
         fig.savefig(str(OUTPATH) + "_ic-background.png")
 
 # estimate frequency of oscillations
-# TODO switch lmfit model to take area instead of radians_per_tesla
 if "LOOP_AREA_GUESS" in config:
-    model.set_param_hint(
-        "radians_per_tesla", value=2 * np.pi * config.getfloat("LOOP_AREA_GUESS") / PHI0
-    )
+    model.set_param_hint("loop_area", value=config.getfloat("LOOP_AREA_GUESS"))
 cyc_per_T, (freqs, fft) = estimate_frequency(
     bfield, ic_p if args.branch in {"+", "+-"} else ic_n
 )
@@ -328,9 +325,9 @@ if args.fraunhofer == "filter":
     ic_filtered = np.fft.irfft(fft_filt, n=len(bfield))
     cyc_per_T, (freqs_filt, fft_filt) = estimate_frequency(bfield, ic_filtered)
 if "LOOP_AREA_GUESS" not in config:
-    model.set_param_hint("radians_per_tesla", value=2 * np.pi * cyc_per_T)
+    model.set_param_hint("loop_area", value=cyc_per_T * PHI0)
 
-# plot the radians_per_tesla estimate and fraunhofer filter
+# plot the frequency estimate and fraunhofer filter
 abs_fft = np.abs(fft)
 fig, ax = plot(
     freqs / 1e3,
@@ -483,7 +480,7 @@ if args.emcee:
 
 # plot the best fit over the data
 popt = result.params.valuesdict()
-phase = (bfield - popt["bfield_offset"]) * popt["radians_per_tesla"]
+phase_ext = 2 * np.pi * popt["loop_area"] * (bfield - popt["bfield_offset"]) / PHI0
 if args.branch == "+":
     fig, ax_p = plt.subplots()
     ax_p.set_xlabel("$\Phi_\mathrm{ext}$ ($\Phi_0$)")
@@ -498,7 +495,7 @@ else:  # args.branch == "+-"
     best_p, best_n = result.best_fit
 if args.branch in {"+", "+-"}:
     plot(
-        phase / (2 * np.pi),
+        phase_ext / (2 * np.pi),
         ic_p / 1e-6,
         ax=ax_p,
         ylabel="switching current (μA)",
@@ -507,11 +504,11 @@ if args.branch in {"+", "+-"}:
         linewidth=0,
     )
     if not args.dry_run:
-        plot(phase / (2 * np.pi), best_p / 1e-6, ax=ax_p, label="fit")
+        plot(phase_ext / (2 * np.pi), best_p / 1e-6, ax=ax_p, label="fit")
     ax_p.legend()
 if args.branch in {"-", "+-"}:
     plot(
-        phase / (2 * np.pi),
+        phase_ext / (2 * np.pi),
         ic_n / 1e-6,
         ax=ax_n,
         ylabel="switching current (μA)",
@@ -520,7 +517,7 @@ if args.branch in {"-", "+-"}:
         linewidth=0,
     )
     if not args.dry_run:
-        plot(phase / (2 * np.pi), best_n / 1e-6, ax=ax_n, label="fit")
+        plot(phase_ext / (2 * np.pi), best_n / 1e-6, ax=ax_n, label="fit")
     ax_n.legend()
 fig.savefig(str(OUTPATH) + "_fit.png")
 
@@ -529,7 +526,7 @@ if not args.dry_run:
     DataFrame(
         {
             "bfield": bfield,
-            "phase": phase,
+            "phase_ext": phase_ext,
             **(
                 {"ic_p": ic_p, "fit_p": best_p, "init_p": init_p}
                 if args.branch in {"+", "+-"}

commit 6abcad61a816ca642b436f1df492dd4544ef1755

Remove unnecessary bounds on fit parameters
Setting loop_area ~ e-12 so close its minimum bound was freezing the fit
along that dimension.  (This regression was introduced in
bd4321ed4e604250fe0ffc4de6012367647e46ad.)  Other bounds were also
removed to avoid similar issues mapping infinite variables to finite
bounds.  Now negative fit values should indicate an error/bad fit.

shabanipy/squid/lmfitmodels.py
@@ -106,14 +106,11 @@ class SquidModel(Model):
 
     # default parameter specifications
     param_specs = {
-        "loop_area": {"min": 0},
         "anomalous_phase1": {"value": 0, "vary": False},
         "anomalous_phase2": {"value": 0, "vary": False},
-        "critical_current1": {"min": 0},
-        "critical_current2": {"min": 0},
         "transparency1": {"min": 0, "max": 1},
         "transparency2": {"min": 0, "max": 1},
-        "inductance": {"value": 0, "min": 0, "vary": False},
+        "inductance": {"value": 0, "vary": False},
         "temperature": {"vary": False},
         "gap": {"vary": False},
         "nbrute": {"value": 101, "vary": False},