beee3bfaa9e5c06806507b95ec237e18dc9a28a9
Bassel Heiba Elfeky <bhe220@nyu.edu>
2022-09-01 10:52:53 -0400

    Redefining flux functions and adding gaussian convolution to histogram plotting

--- shabanipy/resonators/qpp_analysis.py
+++ shabanipy/resonators/qpp_analysis.py
@@ -13,3 +13,91 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+def FluxFunc_ko1(fl,q0):
+    return (1+q0*np.sin(np.pi*fl/2)*np.arctanh(np.sin(np.pi*fl/2))/(1-np.sin(np.pi*fl/2)*np.arctanh(np.sin(np.pi*fl/2))))**(-0.5)
+
+def multiFluxFunc_ko1(fl,w0,q0,fl0,fs):
+    return w0*(1+q0*np.sin(np.pi*(fl-fl0)/fs/2)*np.arctanh(np.sin(np.pi*(fl-fl0)/fs/2))/(1-np.sin(np.pi*(fl-fl0)/fs/2)*np.arctanh(np.sin(np.pi*(fl-fl0)/fs/2))))**(-0.5)
+    
+def multiFluxFunc_general(fl,w0,q0,fl0,fs):
+    return 
+
+def qpp_expectedShift(q0, f0, ls, phi, delta=170e-6):
+
+    '''Returns the maximum expected shift (tau = 1) due to single quasiparticle
+
+        trapping
+
+
+
+    returns df in linear frequency
+
+    -------------------------------
+
+    q0:     participation ratio
+
+    f0:     zero qp resonance at the given flux [in Hz]
+
+    ls:     the Josephson current of nanosquid (Lj/2)
+
+    phi:    reduced flux -- where you're monitoring
+
+    delta:  superconducting gap in eV
+
+    '''
+
+    phi0 = hbar/2/e
+
+    return (-q0 * f0 * ls * delta * e *
+
+            (np.cos(pi * phi) + (np.sin(pi * phi / 2)**4)) / 8 /
+
+            (phi0**2) / (1 - (np.sin(pi * phi / 2)**2)))
+
+
+
+
+
+def f_n_phi(phi, n, L=1.82897e-9, C=0.739929e-12, Ls=20.8475e-12,
+
+            Delta=2.72370016e-23):
+
+    '''Returns the expected frequency with n trapped quasiparticles
+
+
+
+    returns f(phi,n) in linear frequency
+
+    -------------------------------
+
+    phi:    reduced flux -- where you're monitoring
+
+    n:      number of trapped quasiparticles
+
+    L:      linear inductance
+
+    C:      linear capacitance
+
+    Ls:     squid inductance at 0 flux
+
+    Delta:  superconducting gap in J
+
+    '''
+
+    de = np.pi*phi
+
+    Lsphi = Ls/(1-np.sin(de/2)*np.arctanh(np.sin(de/2)))
+
+    q = Lsphi/(L+Lsphi)
+
+    rphi0 = (2.06783383*1e-15)/(2*np.pi)
+
+    f0 = 1/(2*np.pi*np.sqrt((L+Lsphi)*C))
+
+    alpha = Delta/(2*(rphi0**2))
+
+    # L1 = alpha*(np.cos(de)+np.sin(de/2)**4)/((1-np.sin(de/2)**2)**1.5)
+
+    L1 = alpha*np.cos(de/2)
+
+    return f0 - (q*f0*Lsphi*n*L1/2)
--- shabanipy/resonators/timedomain_analysis.py
+++ shabanipy/resonators/timedomain_analysis.py
@@ -12,11 +12,12 @@ from typing import Optional
 
 import os
 import numpy as np
+from scipy.signal import windows, oaconvolve
 import matplotlib.pyplot as plt
 from shabanipy.plotting import jy_pink
 jy_pink.register()
 
-def histogram_iq(
+def plot_histogram_iq(
     i:np.ndarray,
     q:np.ndarray,
     bins:np.ndarray,
@@ -24,10 +25,48 @@ def histogram_iq(
     fig_size: Optional[np.ndarray] = None,
 )-> None:
 
-    fig, ax = plt.subplots(figsize = (20,10),constrained_layout=True, dpi = 50)
+    fig, ax = plt.subplots(figsize = (14,10),constrained_layout=True, dpi = 50)
     hist = ax.hist2d(i,q, bins = bins, 
                 vmin = counts_lims[0] if counts_lims else 0,
                 vmax = counts_lims[1] if counts_lims else None)
     ax.set_xlabel('I (mV)')
     ax.set_ylabel('Q (mV)')
     cb = fig.colorbar(hist[3], ax=ax)
+
+def gaussian_convolution(data,avgTime,sampleRate):
+
+    '''Smooths data by convolving with a gaussian window of duration avgTime
+
+        and standard deviation avgTime/7.
+
+        returns smoothed data with original shape.
+
+        -------------------------------------
+
+        data:       single or dual channel data. dual channel should have shape (nSamples,2)
+
+        avgTime:    duration of Hann window in seconds
+
+        sampleRate: sample rate of data in Hz.
+
+    '''
+
+    nAvg = int(max(avgTime*sampleRate,1))
+
+    window = windows.gaussian(nAvg,nAvg/7)
+
+    norm = sum(window)
+
+    if len(data.shape) == 2:
+
+        mean = np.mean(data,axis=1)
+
+        window = np.vstack((window,window))
+
+        return (oaconvolve((data.T-mean).T,window,mode='same',axes=1).T/norm + mean).T
+
+    else:
+
+        mean = np.mean(data)
+
+        return oaconvolve(data-mean,window,mode='same')/norm + mean
