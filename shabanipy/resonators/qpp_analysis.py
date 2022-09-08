1c988a80db9c6b054dd5e5f2f0e50441bb836392
Bassel Heiba Elfeky <bhe220@nyu.edu>
2022-09-08 11:15:44 -0400

    Adding multifluxfunc_general and fluxfunc_general which are generalizied version of the multifluxfunc and fluxfunc originally written for  KO1 CPR

--- shabanipy/resonators/qpp_analysis.py
+++ shabanipy/resonators/qpp_analysis.py
@@ -10,17 +10,25 @@
 """
 
 import os
+from typing import Optional
 import numpy as np
 import matplotlib.pyplot as plt
+from shabanipy.plotting import jy_pink
+jy_pink.register()
+plt.style.use('bold')
+plt.style.use('presentation')
 
 def FluxFunc_ko1(fl,q0):
     return (1+q0*np.sin(np.pi*fl/2)*np.arctanh(np.sin(np.pi*fl/2))/(1-np.sin(np.pi*fl/2)*np.arctanh(np.sin(np.pi*fl/2))))**(-0.5)
 
+def FluxFunc_general(fl,q0,t):
+    return (1+q0*((t*np.cos(np.pi*fl)-t+2)**1.5/(2**0.5*(t/4*(np.cos(2*np.pi*fl)+3)-(t-2)*np.cos(np.pi*fl))) - 1))**(-0.5)
+
 def multiFluxFunc_ko1(fl,w0,q0,fl0,fs):
     return w0*(1+q0*np.sin(np.pi*(fl-fl0)/fs/2)*np.arctanh(np.sin(np.pi*(fl-fl0)/fs/2))/(1-np.sin(np.pi*(fl-fl0)/fs/2)*np.arctanh(np.sin(np.pi*(fl-fl0)/fs/2))))**(-0.5)
     
-def multiFluxFunc_general(fl,w0,q0,fl0,fs):
-    return 
+def multiFluxFunc_general(fl,w0,q0,fl0,fs, t):
+    return w0*(1+q0*((t*np.cos(np.pi*(fl-fl0)/fs)-t+2)**(1.5)/(2**(0.5)*(t/4*(np.cos(2*np.pi*(fl-fl0)/fs)+3)-(t-2)*np.cos(np.pi*(fl-fl0)/fs))) - 1))**(-0.5)
 
 def qpp_expectedShift(q0, f0, ls, phi, delta=170e-6):
 
@@ -101,3 +109,29 @@ def f_n_phi(phi, n, L=1.82897e-9, C=0.739929e-12, Ls=20.8475e-12,
     L1 = alpha*np.cos(de/2)
 
     return f0 - (q*f0*Lsphi*n*L1/2)
+
+def plot_clearing_tone(
+    tone_freq:np.ndarray,
+    freq:np.ndarray,
+    s21:np.ndarray,
+    cb_lims: Optional[np.ndarray] = None,
+    fig_size: Optional[np.ndarray] = None,
+)-> None:
+
+    fig, ax = plt.subplots(figsize = (20,10),constrained_layout=True, dpi = 50)
+    m_ax = fig.gca()
+    pm = m_ax.pcolormesh(tone_freq/1e9,
+            freq/1e9,
+            np.abs(s21),
+            vmin = cb_lims[0] if cb_lims else  0,
+            vmax = cb_lims[1] if cb_lims else  None,
+            cmap = 'jy_pink', 
+            shading = 'auto',
+            linewidth=0,
+            rasterized = True
+            )
+    cb = fig.colorbar(pm, ax = m_ax,pad = 0.02)
+    cb.ax.set_xlabel(r'S21', labelpad = 10)
+    m_ax.set_xlabel('Clearing tone frequency (GHz)', labelpad = 20)
+    m_ax.set_ylabel('Freq (GHz)')
+    # cb = fig.colorbar(hist[3], ax=ax)
