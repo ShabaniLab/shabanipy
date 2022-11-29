8586501cd9d6692fb3560d1f6b07b5404cdedb4c
William Schiela <bschiela@gmail.com>
2022-11-28 21:36:33 -0500

    Maintain CPR-fitting script
    Use configparser machinery now refactored into shabanipy library and get
    the script running again.

--- scripts/squid/JS512-C09_FlSq4to1-8/fit_cpr.py
+++ scripts/squid/JS512-C09_FlSq4to1-8/fit_cpr.py
@@ -14,6 +14,7 @@ idler junction is assumed to be phase-fixed, contributing a constant offset.
 import argparse
 import warnings
 from contextlib import redirect_stdout
+from importlib import import_module
 from pathlib import Path
 
 import numpy as np
@@ -22,15 +23,17 @@ from matplotlib import pyplot as plt
 from scipy.constants import eV
 
 from shabanipy.dvdi import extract_switching_current, find_rising_edge
-from shabanipy.labber import LabberData
-from shabanipy.squid.cpr import finite_transparency_jj_current as cpr
-from shabanipy.utils.plotting import plot, plot2d, jy_pink
+from shabanipy.jj import transparent_cpr as cpr
+from shabanipy.labber import LabberData, get_data_dir
+from shabanipy.utils import load_config, plot, plot2d
+from shabanipy.utils.plotting import jy_pink
 
 # set up the command-line interface
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument(
     "config_path", help="path to a config file, relative to this script."
 )
+parser.add_argument("config_section", help="section of the .ini config file to use")
 parser.add_argument(
     "--plot-guess",
     "-g",
@@ -40,20 +43,39 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-# load the config file
-with open(Path(__file__).parent / args.config_path) as f:
-    print(f"Using config file `{f.name}`")
-    exec(f.read())
+_, config = load_config(args.config_path, args.config_section)
 
-Path(OUTDIR).mkdir(parents=True, exist_ok=True)
+inpath = Path(config.get("LABBERDATA_DIR", get_data_dir())) / config["DATAPATH"]
+outdir = Path("output/")
+outdir.mkdir(parents=True, exist_ok=True)
+outpath = outdir / f"{config['COOLDOWN']}-{config['SCAN']}"
+
+jy_pink.register()
 plt.style.use(["jy_pink", "fullscreen13"])
 
+# magnet coil current-per-field conversion factor
+if "Source current" in config["CH_FIELD_PERP"]:
+    print("Scaling magnet source current to field")
+    AMPS_PER_T = getattr(
+        import_module("shabanipy.constants"),
+        f"{config['FRIDGE'].upper()}_AMPS_PER_TESLA_{config['PERP_AXIS'].upper()}",
+    )
+else:
+    AMPS_PER_T = 1
+
+# sanity check conversion factor is correct (relies on my local file hierarchy)
+if config["FRIDGE"] not in str(inpath):
+    warnings.warn(
+        f"I can't double check that {config['DATAPATH']} is from {config['FRIDGE']}"
+    )
+
 # load the data
-with LabberData(DATAPATH) as f:
-    bfield = f.get_data(CHAN_FIELD_PERP) / AMPS_PER_T
-    ibias, lockin = f.get_data(CHAN_LOCKIN, get_x=True)
+with LabberData(inpath) as f:
+    bfield = f.get_data(config["CH_FIELD_PERP"]) / AMPS_PER_T
+    ibias, lockin = f.get_data(config["CH_LOCKIN"], get_x=True)
     dvdi = np.abs(lockin)
-    temp_meas = f.get_data(CHAN_TEMP_MEAS)
+    temp_meas = f.get_data(config["CH_TEMP_MEAS"])
+
 
 # check for significant temperature deviations
 MAX_TEMPERATURE_STD = 1e-3
@@ -70,28 +92,32 @@ fig, ax = plot2d(
     ylabel="dc bias (μA)",
     zlabel="dV/dI (Ω)",
     title="raw data",
-    stamp=COOLDOWN_SCAN,
+    stamp=f"{config['COOLDOWN']}-{config['SCAN']}",
 )
-fig.savefig(str(OUTPATH) + "_raw-data.png")
+fig.savefig(str(outpath) + "_raw-data.png")
 
 # extract the switching currents
 ic_n, ic_p = extract_switching_current(
-    ibias, dvdi, side="both", threshold=RESISTANCE_THRESHOLD, interp=True,
+    ibias,
+    dvdi,
+    side="both",
+    threshold=config.getfloat("RESISTANCE_THRESHOLD", fallback=None),
+    interp=True,
 )
 ax.set_title("$I_c$ extraction")
 plot(bfield / 1e-3, ic_p / 1e-6, ax=ax, color="w", lw=0, marker=".")
 plot(bfield / 1e-3, ic_n / 1e-6, ax=ax, color="w", lw=0, marker=".")
-fig.savefig(str(OUTPATH) + "_ic-extraction.png")
+fig.savefig(str(outpath) + "_ic-extraction.png")
 
 # in vector10, positive Bx points into the daughterboard
-if FRIDGE == "vector10":
+if config["FRIDGE"] == "vector10":
     bfield = np.flip(bfield) * -1
     ic_p = np.flip(ic_p)
 # in vector9, positive Bx points out of the daughterboard
-elif FRIDGE == "vector9":
+elif config["FRIDGE"] == "vector9":
     pass
 else:
-    warnings.warn(f"I don't recognize fridge `{FRIDGE}`")
+    warnings.warn(f"I don't recognize fridge `{config['FRIDGE']}`")
 
 # parameterize the fit
 params = Parameters()
@@ -116,7 +142,7 @@ fig, ax = plot(
 )
 ax.axhline(params["ic_offset"] / 1e-6, color="black")
 ax.axvline(params["bfield_offset"] / 1e-3, color="black")
-fig.savefig(str(OUTPATH) + "_bfield-offset.png")
+fig.savefig(str(outpath) + "_bfield-offset.png")
 
 # guess the field-to-phase factor by FFT
 dbs = np.unique(np.diff(bfield))
@@ -138,7 +164,7 @@ fig, ax = plot(
     xlabel="frequency [mT$^{-1}$]",
     ylabel="|FFT| [arb. u.]",
     title="frequency guess",
-    stamp=COOLDOWN_SCAN,
+    stamp=f"{config['COOLDOWN']}-{config['SCAN']}",
     marker="o",
 )
 ax.axvline(freq_guess / 1e3, color="k")
@@ -149,7 +175,7 @@ ax.text(
     f"period = {round(1 / freq_guess / 1e-6)} μT",
     transform=ax.transAxes,
 )
-fig.savefig(str(OUTPATH) + "_fft.png")
+fig.savefig(str(outpath) + "_fft.png")
 
 # define the model to fit
 def model(params, bfield):
@@ -176,9 +202,9 @@ mini = Minimizer(residuals, params, fcn_args=(bfield, ic_p))
 result = mini.minimize()
 print("done.")
 print(fit_report(result))
-with open(str(OUTPATH) + "_fit-report.txt", "w") as f:
+with open(str(outpath) + "_fit-report.txt", "w") as f:
     f.write(fit_report(result))
-with open(str(OUTPATH) + "_fit-params.txt", "w") as f:
+with open(str(outpath) + "_fit-params.txt", "w") as f:
     with redirect_stdout(f):
         result.params.pretty_print(precision=8)
 
@@ -192,7 +218,7 @@ fig, ax = plot(
     xlabel="phase [2π]",
     ylabel="switching current [μA]",
     label="data",
-    stamp=COOLDOWN_SCAN,
+    stamp=f"{config['COOLDOWN']}-{config['SCAN']}",
 )
 if args.plot_guess:
     plot(
@@ -201,6 +227,6 @@ if args.plot_guess:
 plot(
     phase / (2 * np.pi), model(result.params, bfield) / 1e-6, ax=ax, label="fit",
 )
-fig.savefig(str(OUTPATH) + "_fit.png")
+fig.savefig(str(outpath) + "_fit.png")
 
 plt.show()
