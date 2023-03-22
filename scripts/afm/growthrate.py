90cd397037b40569b2abb7535f8e8376b67ebaba
William Schiela <bschiela@gmail.com>
2023-03-22 13:30:08 -0400

    Add option to specify uncertainties in fitted data

--- scripts/afm/growthrate.py
+++ scripts/afm/growthrate.py
@@ -31,19 +31,29 @@ parser.add_argument(
     action="store_true",
     help="force the fit to intersect the origin",
 )
+parser.add_argument(
+    "--error-bars",
+    "-e",
+    default=False,
+    action="store_true",
+    help="use error bars in the fit",
+)
 args = parser.parse_args()
 df = read_csv(args.path, sep="\t")
 time, thickness, sigma = df.T.to_numpy()
 
 plt.style.use("fullscreen13")
-plt.plot(time, thickness, "o", markersize=10)
+if args.error_bars:
+    plt.errorbar(time, thickness, yerr=sigma, fmt="o", capsize=10)
+else:
+    plt.plot(time, thickness, "o", markersize=10)
 plt.xlabel(df.columns[0])
 plt.ylabel(df.columns[1])
 
 model = LinearModel()
 if args.force_zero:
     model.set_param_hint("intercept", value=0, vary=False)
-result = model.fit(thickness, x=time)
+result = model.fit(thickness, x=time, weights=1 / sigma if args.error_bars else None)
 time_extrap = np.insert(time, 0, 0)
 plt.plot(time_extrap, result.eval(x=time_extrap))
 slope, intercept = result.params.valuesdict().values()
