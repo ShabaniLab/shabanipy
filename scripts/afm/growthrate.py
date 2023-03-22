b9f2ffc709b2f62018fa5c93badd5f596b6be2ea
William Schiela <bschiela@gmail.com>
2023-03-22 13:11:52 -0400

    Extract growth rate from linear fit

---
+++ scripts/afm/growthrate.py
@@ -0,0 +1,43 @@
+"""Calculate growth rate from a linear fit of thickness vs. time.
+
+The input to this script is a tab-separated file with deposition time, measured
+thickness, and thickness uncertainty in the following format:
+
+```
+time	thickness	sigma
+500	10.0	4.0
+1000	20.0	5.0
+...
+```
+"""
+
+import argparse
+
+import numpy as np
+from lmfit.models import LinearModel
+from matplotlib import pyplot as plt
+from pandas import read_csv
+
+parser = argparse.ArgumentParser(
+    description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
+parser.add_argument(
+    "path", help="path to the .txt histogram data file exported from NMI AFM software"
+)
+args = parser.parse_args()
+df = read_csv(args.path, sep="\t")
+time, thickness, sigma = df.T.to_numpy()
+
+plt.style.use("fullscreen13")
+plt.plot(time, thickness, "o", markersize=10)
+plt.xlabel(df.columns[0])
+plt.ylabel(df.columns[1])
+
+model = LinearModel()
+result = model.fit(thickness, x=time)
+time_extrap = np.insert(time, 0, 0)
+plt.plot(time_extrap, result.eval(x=time_extrap))
+slope, intercept = result.params.valuesdict().values()
+plt.title(f"{slope=:.5f}, {intercept=:.1f}")
+plt.savefig("growthrate.png")
+plt.show()
