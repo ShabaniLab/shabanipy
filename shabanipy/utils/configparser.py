01949bddb70002b3bc0b7ab73f0826bcfaee81fb
William Schiela <bschiela@gmail.com>
2023-06-16 20:28:25 -0400

    Pretty print available config sections

--- shabanipy/utils/configparser.py
+++ shabanipy/utils/configparser.py
@@ -2,6 +2,7 @@
 
 from configparser import ConfigParser, ExtendedInterpolation, SectionProxy
 from pathlib import Path
+from pprint import pformat
 from typing import Optional, Tuple, Union
 
 
@@ -31,7 +32,7 @@ def load_config(
                 config = ini[section]
             except KeyError as e:
                 raise ValueError(
-                    f"'{section}' not found. Available sections are {ini.sections()}"
+                    f"'{section}' not found. Available sections are:\n{pformat(ini.sections())}"
                 ) from e
         else:
             config = None
