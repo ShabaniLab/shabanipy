ShabaniLab/shabanipy

commit 7e242b5e39b0156998785e3551c84f37f4685f5a

Handle interrupted scans in shablabber
Use the shape of the data that was actually recorded, rather than the
intended shape implied by the step dimensions.

shabanipy/labber/shablabber.py
@@ -60,13 +60,15 @@ class ShaBlabberFile(File):
 
     @property
     def _step_dims(self) -> Tuple[int]:
-        """Step dimensions, i.e. the shape of the data."""
+        """Step dimensions, i.e. number of points for each step channel."""
         return tuple(self.attrs["Step dimensions"])
 
     @cached_property
     def _shape(self) -> Tuple[int]:
         """The shape of the data."""
-        return self._trace_dims + tuple(d for d in self._step_dims if d != 1)
+        data_shape = list(self["Data/Data"].shape)
+        del data_shape[1]  # number of channels
+        return self._trace_dims + tuple(data_shape)
 
     @property
     def comment(self) -> str:

commit 793ec104785ecea05b96d41e2e6ba6a049e80db4

Fix shape of 3d+ data
Regression was introduced in 7e242b5e39b0156998785e3551c84f37f4685f5a.

I assumed Labber saved n-dimensional data in the shape

    (n1, n_ch, n2, n3, ..., nd)

where n_ch is the number of channel columns and (n1, ..., nd) are the
dimensions of the data.  In fact, Labber saves the data in the 3d shape

    (n1, n_ch, n2 * n3 * ... * nd)

See this scan for an example:
vector9/2023/06/Data_0618/JS633-W2_Wsc@v3.1_Wsc=0.6um-NNW_WFS01-177.hdf5

For interrupted scans, we currently infer only the last dimension.  It's
hard to cover all corner cases without seeing more examples of how
Labber handles interrupted scans.

shabanipy/labber/shablabber.py
@@ -58,6 +58,11 @@ class ShaBlabberFile(File):
         """The date and time the hdf5 file was created."""
         return datetime.fromtimestamp(self.attrs["creation_time"])
 
+    @cached_property
+    def _completed(self) -> bool:
+        """Did the scan finish, or was it aborted or crashed."""
+        return self["Data"].attrs["Completed"]
+
     @property
     def _step_dims(self) -> Tuple[int]:
         """Step dimensions, i.e. number of points for each step channel."""
@@ -66,10 +71,15 @@ class ShaBlabberFile(File):
     @cached_property
     def _shape(self) -> Tuple[int]:
         """The shape of the data."""
-        data_shape = list(self["Data/Data"].shape)
-        del data_shape[1]  # number of channels/columns
-        data_shape = [dim for dim in data_shape if dim != 1]
-        return self._trace_dims + tuple(data_shape)
+        step_dims = [d for d in self._step_dims if d != 1]
+        if self._completed:
+            return self._trace_dims + tuple(step_dims)
+        else:  # scan aborted/crashed/interrupted
+            # infer the last dimension from the available data
+            data_shape = list(self["Data/Data"].shape)
+            del data_shape[1]  # number of channels/columns
+            step_dims[-1] = np.prod(data_shape) // np.prod(step_dims[:-1])
+            return self._trace_dims + tuple(step_dims)
 
     @property
     def comment(self) -> str: