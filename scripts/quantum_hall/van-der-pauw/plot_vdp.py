5585a798a90b654d75fd96edb512c4e991ee174a
smfarzaneh <smf1992@gmail.com>
2023-06-23 10:33:53 -0400

    cosmetic changes

--- scripts/quantum_hall/van-der-pauw/plot_vdp.py
+++ scripts/quantum_hall/van-der-pauw/plot_vdp.py
@@ -29,26 +29,29 @@ fig, ax = plt.subplots(1, 1, dpi=600)
 ax.plot(z_field_xx, rxx/1e3, 
     color=colors[0],
     linewidth=1,
-    label=r"$R_{xx}$")
+    label=r"$R_{xx}$" + r", $\mu_{xx} = $" + r"{0:.2e}".format(mobility_xx[0]*1e4) + " cm$^2$/Vs")
 ax.plot(z_field_yy, ryy/1e3, 
     color=colors[1],
     linewidth=1,
-    label=r"$R_{yy}$")
+    label=r"$R_{yy}$" + r", $\mu_{yy} = $" + r"{0:.2e}".format(mobility_yy[0]*1e4) + " cm$^2$/Vs")
+ax.plot([], [], 
+    color=colors[-1],
+    linewidth=1,
+    label=r"$R_{xy}$" + r", $n$ = {0:.2e}".format(density[0]/1e4) + " cm$^{-2}$")
 ax2 = ax.twinx()
 ax2.plot(z_field_xy, rxy/1e3, 
     color=colors[-1],
-    linewidth=1,
-    label=r"$R_{xy}$")
-ax.legend()
+    linewidth=1)
+ax.legend(loc=2)
 ax.yaxis.label.set_color(colors[0]) 
 ax2.yaxis.label.set_color(colors[-1]) 
 # ax2.tick_params(colors=colors[-1])
 ax.set_xlim(z_field_xx[0], z_field_xx[-1])
-ax.set_xlabel(r"$H$ (T)")
+ax.set_xlabel(r"$H_z$ (T)")
 ax.set_ylabel(r"$R_{xx}, R_{yy}$ (k$\Omega$)")
 ax2.set_ylabel(r"$R_{xy}$ (k$\Omega$)")
 name = Loader.get_name()
-ax.set_title(name + r", $n$ = {0:.2e}".format(density[0]/1e4) + "cm$^{-2}$, $\mu_{xx}$ =" + r"{0:.2e}".format(mobility_xx[0]*1e4) + " cm$^2$/Vs, $\mu_{yy}$ =" + r"{0:.2e}".format(mobility_yy[0]*1e4) + " cm$^2$/Vs")
+ax.set_title(name)
 fig.set_size_inches(5, 5)
 path = f"{name}.pdf"
 plt.savefig(path, bbox_inches='tight') 
