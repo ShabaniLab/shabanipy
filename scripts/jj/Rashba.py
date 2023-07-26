6f1967ea2fe82bffa6695e313b2055b1a15daebc
William Schiela <bschiela@gmail.com>
2023-07-25 21:39:39 -0400

    Run autoflake, isort, and black

--- scripts/jj/Rashba.py
+++ scripts/jj/Rashba.py
@@ -4,22 +4,22 @@ This script fits Ic+(B||) and Ic-(B||) according to Eq. (1) of
 https://arxiv.org/abs/2303.01902v2.
 """
 import argparse
-from warnings import warn
-from pprint import pprint
 from pathlib import Path
+from pprint import pprint
+from warnings import warn
 
 import matplotlib.pyplot as plt
 import numpy as np
-from pandas import read_csv
-from scipy.optimize import curve_fit
 from lmfit import Model
+from pandas import read_csv
 
 # set up command-line interface
 parser = argparse.ArgumentParser(
     description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
 )
 parser.add_argument(
-    "datapath", help="path to .csv file containing positive & negative switching current vs. field"
+    "datapath",
+    help="path to .csv file containing positive & negative switching current vs. field",
 )
 parser.add_argument(
     "--bmax",
@@ -31,15 +31,19 @@ args = parser.parse_args()
 
 # extract data
 df = read_csv(args.datapath)
-icp = np.abs(df['ic+'].values)
-icm = np.abs(df['ic-'].values)
+icp = np.abs(df["ic+"].values)
+icm = np.abs(df["ic-"].values)
 # field column name might vary
 colname = [c for c in df.columns if "field" in c.lower()]
 if len(colname) == 1:
     bfield = df[colname[0]].values
 else:
     pprint({i: colname for i, colname in enumerate(df.columns)})
-    colindex = int(input(f"Which column has the field data? [select index 0-{len(df.columns) - 1}]: "))
+    colindex = int(
+        input(
+            f"Which column has the field data? [select index 0-{len(df.columns) - 1}]: "
+        )
+    )
     bfield = df.iloc[:, colindex].values
 
 # limit field range
@@ -54,18 +58,22 @@ if args.bmax is not None:
 if icp[-1] < icm[-1]:
     icp, icm = icm, icp
 
+
 # build model from https://arxiv.org/abs/2303.01902v2 Eq. (1)
 def icp_model(x, imax, b, c, bstar):
     """Positive critical current, Ic+."""
     return imax * (1 - b * (1 + c * np.sign(x - bstar)) * (x - bstar) ** 2)
 
+
 def icm_model(x, imax, b, c, bstar):
     """Negative critical current, Ic-."""
     return imax * (1 - b * (1 - c * np.sign(x + bstar)) * (x + bstar) ** 2)
 
+
 def icpm_model(x, imax, b, c, bstar):
     """Positive and negative critical current, Ic+-."""
     return np.concatenate([f(x, imax, b, c, bstar) for f in (icp_model, icm_model)])
+
 
 model = Model(icpm_model)
 model.set_param_hint("imax", value=np.mean([icp.max(), icm.max()]), vary=False)
@@ -94,12 +102,12 @@ n = 100
 bfield_smooth = np.linspace(bfield.min(), bfield.max(), n)
 fit = model.eval(result.params, x=bfield_smooth)
 plt.style.use(["fullscreen13"])
-plt.plot(bfield / 1e-3, icp / 1e-6, 'o', label='$I_{c+}$ data', color="tab:blue")
-plt.plot(bfield / 1e-3, icm / 1e-6, 'o', label='$I_{c-}$ data', color="tab:orange")
-plt.plot(bfield_smooth / 1e-3, fit[:n] / 1e-6, label='$I_{c+}$ fit', color="tab:blue")
-plt.plot(bfield_smooth / 1e-3, fit[n:] / 1e-6, label='$I_{c-}$ fit', color="tab:orange")
-plt.xlabel('in-plane field (mT)')
-plt.ylabel('critical current (μA)')
+plt.plot(bfield / 1e-3, icp / 1e-6, "o", label="$I_{c+}$ data", color="tab:blue")
+plt.plot(bfield / 1e-3, icm / 1e-6, "o", label="$I_{c-}$ data", color="tab:orange")
+plt.plot(bfield_smooth / 1e-3, fit[:n] / 1e-6, label="$I_{c+}$ fit", color="tab:blue")
+plt.plot(bfield_smooth / 1e-3, fit[n:] / 1e-6, label="$I_{c-}$ fit", color="tab:orange")
+plt.xlabel("in-plane field (mT)")
+plt.ylabel("critical current (μA)")
 plt.legend()
 plt.savefig(outpath + ".png")
 plt.show()
