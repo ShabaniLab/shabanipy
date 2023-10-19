12e0fb3daea8e7929b069523c42121129fd9fe4c
William Schiela <bschiela@gmail.com>
2023-10-19 17:48:36 -0400

    Return DEFAULT config section by default

--- shabanipy/utils/configparser.py
+++ shabanipy/utils/configparser.py
@@ -7,8 +7,9 @@ from typing import Optional, Tuple, Union
 
 
 def load_config(
-    path: Union[str, Path], section: Optional[str] = None
-) -> Tuple[ConfigParser, Optional[SectionProxy]]:
+    path: Union[str, Path],
+    section: Optional[str] = "DEFAULT",
+) -> Tuple[ConfigParser, SectionProxy]:
     """Load a configuration file.
 
     Parameters
@@ -16,24 +17,23 @@ def load_config(
     path
         The path to the config file, i.e. an .ini file in configparser format.
     section (optional)
-        A section of the config file to access.
+        A section of the config file to access.  If None, defaults to DEFAULT.
 
     Returns
     -------
     ini, config
         The root of the .ini file dictionary, and the config section specified by
-        `section`.  If `section` is None, `config` is None.
+        `section`.
     """
     with open(Path(path)) as f:
+        if section is None:
+            section = "DEFAULT"
         ini = ConfigParser(interpolation=ExtendedInterpolation())
         ini.read_file(f)
-        if section is not None:
-            try:
-                config = ini[section]
-            except KeyError as e:
-                raise ValueError(
-                    f"'{section}' not found. Available sections are:\n{pformat(sorted(ini.sections()))}"
-                ) from e
-        else:
-            config = None
+        try:
+            config = ini[section]
+        except KeyError as e:
+            raise ValueError(
+                f"'{section}' not found. Available config sections are:\n{pformat(sorted(ini.sections()))}"
+            ) from e
     return ini, config
