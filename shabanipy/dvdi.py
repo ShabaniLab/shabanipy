Fix index overflow in offset computation
The previous implementation only handled index "underflow" (i.e. not
enough points to the left) and not index "overflow" (i.e. not enough
points to the right).  The unit test was also incorrectly written, as
the function assumes x is sorted.

shabanipy/dvdi.py
@@ -223,7 +223,9 @@ def _compute_offset(x: np.ndarray, y: np.ndarray, n: int) -> np.ndarray:
     x0_index = np.atleast_1d(np.argmin(np.abs(x), axis=-1))
     indexes_to_average = np.concatenate(
         [
-            np.arange(i0 - n // 2, i0 + n // 2 + 1) - min(i0 - n // 2, 0)
+            np.arange(i0 - n // 2, i0 + n // 2 + 1)
+            - min(i0 - n // 2, 0)
+            - max(i0 + n // 2 + 1 - x.shape[-1], 0)
             for i0 in x0_index.flatten()
         ]
     ).reshape(y.shape[:-1] + (-1,))

tests/test_dvdi.py
@@ -166,11 +166,11 @@ class TestComputeOffset(unittest.TestCase):
         assert_array_equal(_compute_offset(x, y, 5), [15 / 5])
 
     def test_compute_offset_negative_bias(self):
-        x = np.array([0, -1, -2, -3, -4])
+        x = np.array([-4, -3, -2, -1, 0])
         y = np.array([1, 2, 3, 4, 5])
-        assert_array_equal(_compute_offset(x, y, 1), [1])
-        assert_array_equal(_compute_offset(x, y, 2), [6 / 3])
-        assert_array_equal(_compute_offset(x, y, 3), [6 / 3])
+        assert_array_equal(_compute_offset(x, y, 1), [5])
+        assert_array_equal(_compute_offset(x, y, 2), [12 / 3])
+        assert_array_equal(_compute_offset(x, y, 3), [12 / 3])
         assert_array_equal(_compute_offset(x, y, 4), [15 / 5])
         assert_array_equal(_compute_offset(x, y, 5), [15 / 5])
 