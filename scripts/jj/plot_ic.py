4d105565ccce1b6d079ed980575ede84201e6e99
William Schiela <bschiela@gmail.com>
2024-06-11 01:16:55 -0400

    Plot error bars in Ic and diode plots

--- scripts/jj/plot_ic.py
+++ scripts/jj/plot_ic.py
@@ -22,6 +22,9 @@ parser = argparse.ArgumentParser(
 parser.add_argument(
     "csv_path", help="path to .csv file output by centermax.py, relative to this script"
 )
+parser.add_argument(
+    "--no-errorbars", help="don't plot errorbars", default=False, action="store_true"
+)
 parser.add_argument("--xmin", help="minimum value of x axis", type=float)
 parser.add_argument("--xmax", help="maximum value of x axis", type=float)
 args = parser.parse_args()
@@ -45,6 +48,11 @@ if args.xmin or args.xmax:
     outprefix = Path(str(outprefix) + f"_{args.xmin}-{args.xmax}")
 x = csv[xname].to_numpy()
 ic = csv[[f"ic{sign} from fit" for sign in branch]].to_numpy()
+if np.all(f"rmse{sign}" in csv for sign in branch) and not args.no_errorbars:
+    errorbars = True
+    rmse = csv[[f"rmse{sign}" for sign in branch]].to_numpy()
+else:
+    errorbars = False
 
 stamp = Path(*Path(args.csv_path).parts[-4:])
 fig, ax = plot(
@@ -56,6 +64,11 @@ fig, ax = plot(
     ylabel="critical current (μA)",
     stamp=stamp,
 )
+if errorbars:
+    for ic_, rmse_ in zip(ic.T, rmse.T):
+        ax.errorbar(
+            x, ic_ / 1e-6, yerr=rmse_ / 1e-6, lw=0, elinewidth=2, color="tab:blue"
+        )
 for i in ic.T:
     ax.fill_between(x, i / 1e-6, color="tab:blue", alpha=0.5)
 ax.set_xlim((x.min(), x.max()))
@@ -72,35 +85,61 @@ if branch == "+-":
         label=label,
         stamp=stamp,
     )
+    if errorbars:
+        for ic_, rmse_, color in zip(ic.T, rmse.T, ("tab:blue", "tab:orange")):
+            ax.errorbar(
+                x,
+                np.abs(ic_) / 1e-6,
+                yerr=rmse_ / 1e-6,
+                color=color,
+                lw=0,
+                elinewidth=2,
+            )
     ax.fill_between(x, *np.abs(ic.T / 1e-6), color="tab:gray", alpha=0.5)
     fig.savefig(str(outprefix) + "_ic-mag.png")
 
     fig, ax = plt.subplots()
     ax.axhline(0, color="k")
     ax.axvline(0, color="k")
+    delta = -np.diff(np.abs(ic)).squeeze()
     plot(
         x,
-        -np.diff(np.abs(ic)).squeeze() / 1e-9,
+        delta / 1e-9,
         ".-",
         xlabel=xname,
         ylabel="$\Delta I_c$ (nA)",
         ax=ax,
         stamp=stamp,
     )
+    if errorbars:
+        delta_err = np.sqrt(np.sum(rmse**2, axis=-1))
+        ax.errorbar(
+            x, delta / 1e-9, yerr=delta_err / 1e-9, color="tab:blue", lw=0, elinewidth=2
+        )
     fig.savefig(str(outprefix) + "_ic-diff.png")
 
     fig, ax = plt.subplots()
     ax.axhline(0, color="k")
     ax.axvline(0, color="k")
+    sigma = np.sum(np.abs(ic), axis=-1)
+    eta = delta / sigma
     plot(
         x,
-        -np.diff(np.abs(ic)).squeeze() / np.sum(np.abs(ic), axis=-1),
+        eta * 100,
         ".-",
         xlabel=xname,
-        ylabel="$\Delta I_c / \Sigma I_c$",
+        ylabel="$\Delta I_c / \Sigma I_c$ (%)",
         ax=ax,
         stamp=stamp,
     )
+    if errorbars:
+        sigma_err = delta_err
+        eta_err = np.abs(eta) * np.sqrt(
+            (delta_err / delta) ** 2 + (sigma_err / sigma) ** 2
+        )
+        ax.errorbar(
+            x, eta * 100, yerr=eta_err * 100, color="tab:blue", lw=0, elinewidth=2
+        )
     fig.savefig(str(outprefix) + "_ic-diff-norm.png")
 
 plt.show()
